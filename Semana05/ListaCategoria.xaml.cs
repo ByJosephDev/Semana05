9f408b44413e4625f95e78a4d0b068b3573ecb59
ByJosephDev <byjosephdev@gmail.com>
2022-09-21 21:59:10 -0500

    Agregar archivos de proyecto.

---
+++ Semana05/ListaCategoria.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Business;
+using Entity;
+using System;
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Lógica de interacción para ListaCategoria.xaml
+    /// </summary>
+    public partial class ListaCategoria : Window
+    {
+        public ListaCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            Cargar();
+        }
+
+        private void Cargar()
+        {
+            BCategoria Bcategoria = null;
+            try
+            {
+                Bcategoria = new BCategoria();
+                dgvCategoria.ItemsSource = Bcategoria.Listar(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Comunicarse con el Administrador");
+               
+            }
+            finally
+            {
+                Bcategoria = null;
+            }
+        }
+
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow manCategoria = new MainWindow(0);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+
+        private void DgvCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idCategoria;
+            var item = (Categoria)dgvCategoria.SelectedItem;
+            if (null == item) return;
+            idCategoria = Convert.ToInt32(item.IdCategoria);
+            MainWindow manCategoria = new MainWindow(idCategoria);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+
+    }
+}
